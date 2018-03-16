51a82b3baae051f8fd6d4bbb0aa4595650663ed9
kingblackmic <37343023+kingblackmic@users.noreply.github.com>
2018-03-16 14:20:22 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,7 +14,7 @@ var prefix = "!";
     let args = message.content.split(' ').slice(1).join(' ');
 if(message.content.split(' ')[0] == prefix + 'bc') {
     if (!args[1]) {
-message.channel.send("**f!bc <message>**");
+message.channel.send("**!bc <message>**");
 return;
 }
         message.guild.members.forEach(m => {
