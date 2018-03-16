7f65b484bca060dfca31d5b047fd3edf052a3e06
kingblackmic <37343023+kingblackmic@users.noreply.github.com>
2018-03-16 11:37:20 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,41 +1,64 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+  client.user.setGame(` By Suhaib #4272 .`,'https://www.twitch.tv/v5bz');
+  console.log('---------------');
+  console.log(' Bot Is Online')
+  console.log('---------------')
 });
+var prefix = "*";
+client.on('message', message => {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
 
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+
+  let args = message.content.split(" ").slice(1);
+
+  if (command == "say") {
+   message.channel.sendMessage(args.join("  "))
   }
 });
-
-client.on('message', function(message) {
-                  if(!message.channel.guild) return;
-    if(message.content ===  'f!color') {
-        if(message.member.hasPermission('MANAGE_ROLES')) {
-            setInterval(function(){})
-            message.channel.send('جاري عمل الالوان |✅')
-        }else{
-            message.channel.send('ما معاك البرمشن المطلوب  |❌')
-            }
-    }
-});
-
 client.on('message', message=>{
-    if (message.content ===  'f!color'){
-              if(!message.channel.guild) return;
+    if (message.content === prefix + 'add-colors'){
+        if (message.channel.guild){
             if (message.member.hasPermission('MANAGE_ROLES')){
                 setInterval(function(){})
                   let count = 0;
                   let ecount = 0;
-        for(let x = 1; x < 141; x++){
+        for(let x = 0; x < 110; x++){
             message.guild.createRole({name:x,
             color: 'RANDOM'})
+      }
+            }else{
+                message.channel.sendMessage('? **You do not have permission to write this command**')
             }
-            }
+        }else{
+            message.channel.sendMessage('?  **This command only in servers**')
+        }
     }
+    if (message.content === prefix + 'de-colors'){
+                if (message.channel.guild){
+            if (message.member.hasPermission('MANAGE_ROLES')){
+                setInterval(function(){})
+                  let count = 0;
+                  let ecount = 0;
+        for(let x = 0; x < 110; x++){
+            message.guild.roles.find('name', x).delete()
+      }
+            }else{
+                message.channel.sendMessage('**You do not have permission to write this command**')
+            }
+        }else{
+            message.channel.sendMessage('**This command only in servers**')
+        }
+    }
+    
+})
+client.on('message', msg => {
+  if (msg.content === '*add-colors') {
+    msg.reply('**تم اضافه الاوان بنجاح**');
+  }
 });
-
 client.login('NDIzMTUzMzcwNTE0MTk0NDMy.DYmL0A.F0u_L8RNxNUuX1RoDB2q0RsQJq4');
