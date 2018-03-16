21c3fa8bbb6f0ec337539dfc477848d48e095adf
kingblackmic <37343023+kingblackmic@users.noreply.github.com>
2018-03-16 11:26:51 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,4 +11,31 @@ client.on('message', msg => {
   }
 });
 
+client.on('message', function(message) {
+                  if(!message.channel.guild) return;
+    if(message.content ===  'f!color 140') {
+        if(message.member.hasPermission('MANAGE_ROLES')) {
+            setInterval(function(){})
+            message.channel.send('جاري عمل الالوان |✅')
+        }else{
+            message.channel.send('ما معاك البرمشن المطلوب  |❌')
+            }
+    }
+});
+
+client.on('message', message=>{
+    if (message.content ===  'f!color 140'){
+              if(!message.channel.guild) return;
+            if (message.member.hasPermission('MANAGE_ROLES')){
+                setInterval(function(){})
+                  let count = 0;
+                  let ecount = 0;
+        for(let x = 1; x < 141; x++){
+            message.guild.createRole({name:x,
+            color: 'RANDOM'})
+            }
+            }
+    }
+});
+
 client.login('NDIzMTUzMzcwNTE0MTk0NDMy.DYmL0A.F0u_L8RNxNUuX1RoDB2q0RsQJq4');
