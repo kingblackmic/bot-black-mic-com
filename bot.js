0cb7f2c2f6dba341817337f8a1d5818e4b9c938d
kingblackmic <37343023+kingblackmic@users.noreply.github.com>
2018-03-17 17:03:36 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -27,7 +27,31 @@ client.on('guildMemberAdd', member => {
         .setTimestamp()
    
       channel.sendEmbed(embed);
-    });
+});
 
+
+client.on("message", message => {
+    var prefix = ".";
+            var args = message.content.substring(prefix.length).split(" ");
+            if (message.content.startsWith(prefix + "clear")) {
+                if (!message.member.hasPermission("MANGE_MESSAGES"))  return;
+ if (!args[1]) {
+                                let embed3 = new Discord.RichEmbed()
+                                .setDescription(".clear <number>")
+                                .setColor("RANDOM")
+                                message.channel.sendEmbed(embed3);
+                            } else {
+                            let messagecount = parseInt(args[1]);
+                            message.channel.fetchMessages({limit: messagecount}).then(messages => message.channel.bulkDelete(messages));
+                                                          message.channel.fetchMessages({limit: messagecount}).then(messages => message.channel.bulkDelete(messages));
+                            message.channel.fetchMessages({limit: messagecount}).then(messages => message.channel.bulkDelete(messages));
+                            let embed4 = new Discord.RichEmbed()
+                                                            .setColor("#008000")
+                                .setDescription(":white_check_mark: | Delete " + args[1] + " Message!")
+                                                                                        message.delete("..");
+                                message.channel.sendEmbed(embed4);
+                            }
+                          }
+});
 
 client.login("NDIzMTUzMzcwNTE0MTk0NDMy.DY0s_A.-JpWdwgAC8dIt2svg1aK-j8M4OU");
