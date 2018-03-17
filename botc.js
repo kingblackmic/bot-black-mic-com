d01a98a552cdfc41c960b5f64db342443b21f8fd
kingblackmic <37343023+kingblackmic@users.noreply.github.com>
2018-03-17 19:55:13 +0400

    Create botc.js

---
+++ botc.js
@@ -0,0 +1,35 @@
+message.member.removeRole(message.guild.roles.find("name", "85"));
+
+message.member.removeRole(message.guild.roles.find("name", "86"));
+
+message.member.removeRole(message.guild.roles.find("name", "87"));
+
+message.member.removeRole(message.guild.roles.find("name", "88"));
+message.member.removeRole(message.guild.roles.find("name", "89"));
+message.member.removeRole(message.guild.roles.find("name", "90"));
+message.member.removeRole(message.guild.roles.find("name", "91"));
+message.member.removeRole(message.guild.roles.find("name", "92"));
+message.member.removeRole(message.guild.roles.find("name", "93"));
+message.member.removeRole(message.guild.roles.find("name", "94"));
+message.member.removeRole(message.guild.roles.find("name", "95"));
+message.member.removeRole(message.guild.roles.find("name", "96"));
+message.member.removeRole(message.guild.roles.find("name", "97"));
+message.member.removeRole(message.guild.roles.find("name", "98"));
+message.member.removeRole(message.guild.roles.find("name", "99"));
+message.member.addRole(message.guild.roles.find("name", "100"));
+
+	const embed = new Discord.RichEmbed()
+ 	.setFooter('Requested by '+message.author.username, message.author.avatarURL)
+   .setDescription(`تم تغير اللون بنجاح`)
+  message.channel.sendEmbed(embed);
+	}
+
+
+  if (message.content === '#colors') {
+      if (!message.channel.guild) return;
+    message.channel.sendFile('URL-Of-colors-Img');
+  }
+	
+});
+
+client.login('Token-Is-Here');
