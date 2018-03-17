b8b670cace8ec5339b96b4526df7e0f80ccb87d0
kingblackmic <37343023+kingblackmic@users.noreply.github.com>
2018-03-17 21:58:11 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -140,7 +140,7 @@ let embed = new Discord.RichEmbed()
 
 
 client.on('message', message => {
-var prefix = "-";
+var prefix = "!";
 
   if (!message.content.startsWith(prefix)) return;
   var args = message.content.split(' ').slice(1);
