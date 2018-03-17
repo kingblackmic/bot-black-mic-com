508ec6ea0ea7eb8e48fabc696f5ff2c285af4473
kingblackmic <37343023+kingblackmic@users.noreply.github.com>
2018-03-17 21:51:53 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -139,55 +139,6 @@ let embed = new Discord.RichEmbed()
 });
 
 
-var Discord = require('discord.io');
-const serverName = 'Hero' // أسم السيرفر
-var roleName = 'tst' // أسم الرتبه
-var bot = new Discord.Client({
-    token: 'NDIzMTUzMzcwNTE0MTk0NDMy.DY0s_A.-JpWdwgAC8dIt2svg1aK-j8M4OU', // TOKEN
-    autorun: true
-});
-function getServerId(){
-    for(var i in bot.servers){
-        if(bot.servers[i].name == serverName){
-            return bot.servers[i].id;
-        }
-    }
-    return -1;
-}
-function getRoleId(){
-    var serverId = getServerId();
-    for(var i in bot.servers[serverId].roles){
-        if(bot.servers[serverId].roles[i].name == roleName){
-            return bot.servers[serverId].roles[i].id;
-        }
-    }
-    return -1;
-}
-function changeColor(hex){
-        var newColor = hex
-        bot.editRole({
-            serverID: getServerId(),
-            roleID: getRoleId(),
-            color: newColor
-        });
-}
-bot.on('ready', function(){
-    var ms = 700 ;
-    var color1 = ['#050505','#3c87c9','#f30202','#FFF700','#FF00EB','#0900FF','#0900FF'];
-    var i = -1;
-    var j = 0;
-    setInterval(function (){
-        if( i == -1 ){
-            j = 1;
-        }
-        if( i == (color1.length)-1 ){
-            j = -1;
-        }
-        i = i+j;
-        changeColor(color1[i]);
-    }, ms);
-}); 
-
 
 
 client.login("NDIzMTUzMzcwNTE0MTk0NDMy.DY0s_A.-JpWdwgAC8dIt2svg1aK-j8M4OU");
