6113ea8bd592c389c6c59165da1021e62212d379
kingblackmic <37343023+kingblackmic@users.noreply.github.com>
2018-03-24 22:52:45 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -132,4 +132,30 @@ if(!message.channel.guild) return;
     }
 });
 
+
+client.on("message", message => {
+    var prefix = ".";
+            var args = message.content.substring(prefix.length).split(" ");
+            if (message.content.startsWith(prefix + "clear")) {
+                if (!message.member.hasPermission("MANGE_MESSAGES"))  return;
+ if (!args[1]) {
+                                let embed3 = new Discord.RichEmbed()
+                                .setDescription(".clear <number>")
+                                .setColor("RANDOM")
+                                message.channel.sendEmbed(embed3);
+                            } else {
+                            let messagecount = parseInt(args[1]);
+                            message.channel.fetchMessages({limit: messagecount}).then(messages => message.channel.bulkDelete(messages));
+                                                          message.channel.fetchMessages({limit: messagecount}).then(messages => message.channel.bulkDelete(messages));
+                            message.channel.fetchMessages({limit: messagecount}).then(messages => message.channel.bulkDelete(messages));
+                            let embed4 = new Discord.RichEmbed()
+                                                            .setColor("#008000")
+                                .setDescription(":white_check_mark: | Delete " + args[1] + " Message!")
+                                                                                        message.delete("..");
+                                message.channel.sendEmbed(embed4);
+                            }
+                          }
+});
+
+
 client.login('NDI3MDk0MzI1NDI0MjkxODYy.DZgA_g.t4K2Rl4CmqWgAGdBLgn7mcMs4sQ');
