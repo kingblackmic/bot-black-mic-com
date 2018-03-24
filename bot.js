262b3df8751c5395a71df3cdbd24d27cf2f7e9a9
kingblackmic <37343023+kingblackmic@users.noreply.github.com>
2018-03-24 19:41:48 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,14 +1,30 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+
+
+client.on("ready", () => {
+client.user.setPresence({
+  status: 'dnd',
+  game: { 
+     type: 0,
+     name: 'Surprise Mother Fucker :) ',
+     details: `I'm : KBOOSH - Developer and Programmer`,
+     url: 'http://twitch.tv/Streammingg',
+     state: `إنْ لَمْ تَجِد لكْ حآقدْ إعلمْ أنْك إنسآن فآشِلْ`,
+    application_id: '281376075802476544',
+     assets: {
+        small_image: `366835431037337600`,
+        small_text: ' Take This ! ' ,
+        large_image: `370451271133429760`,
+        large_text: `ҜṦẰ FOREVER ♥` }
+
+  }
+    });
 });
 
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
-  }
-});
+
+
+ 
 
 client.login('NDI3MDk0MzI1NDI0MjkxODYy.DZgA_g.t4K2Rl4CmqWgAGdBLgn7mcMs4sQ');
