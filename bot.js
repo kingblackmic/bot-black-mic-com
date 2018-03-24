5a3a4406613420994d17f26bec742483fda77758
kingblackmic <37343023+kingblackmic@users.noreply.github.com>
2018-03-24 22:05:45 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -50,7 +50,7 @@ let embed = new Discord.RichEmbed()
 client.on('message', message => {
     var prefix = "!"
     let command = message.content.split(" ")[0];
-  command = command.slice(prefix.length);
+  command = command.slice(prefix + 'length');
 
   let args = message.content.split(" ").slice(1);
 
