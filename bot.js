222133307aeca034c4e4271466830f95b9bb3958
kingblackmic <37343023+kingblackmic@users.noreply.github.com>
2018-03-27 21:51:02 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -168,4 +168,54 @@ client.on('message', message => {
 });
 
 
+
+
+
+
+
+ client.on('message', message => {
+                    var prefix = "!";
+                    
+                      if (!message.content.startsWith(prefix)) return;
+                      var args = message.content.split(' ').slice(1);
+                      var argresult = args.join(' ');
+                      if (message.author.id == 427094325424291862) return;
+                    
+                    
+                    if (message.content.startsWith(prefix + 'playing')) {
+                    if (message.author.id !== '257111476404224001') return message.reply('** هذا الأمر فقط لصاحب البوت و شكراًً **')
+                    client.user.setGame(argresult);
+                        message.channel.sendMessage(`**${argresult}** : تم تغيير الحالة`)
+                    } else
+                    
+                     
+                    if (message.content.startsWith(prefix + 'streem')) {
+                    if (message.author.id !== '257111476404224001') return message.reply('** هذا الأمر فقط لصاحب البوت و شكراًً **')
+                    client.user.setGame(argresult, "http://twitch.tv/SMILE");
+                        message.channel.sendMessage(`**${argresult}** :تم تغيير الحالة الى ستريمنج`)
+                    } else
+                    
+                    if (message.content.startsWith(prefix + 'setname')) {
+                    if (message.author.id !== '257111476404224001') return message.reply('** هذا الأمر فقط لصاحب البوت و شكراًً **')
+                      client.user.setUsername(argresult).then
+                          message.channel.sendMessage(`**${argresult}** : تم تغير الأسم`)
+                      return message.reply("**لا تستطيع تغير الأسم الا بعد ساعتين**");
+                    } else
+
+if (message.content.startsWith(prefix + 'setavatar')) {
+                    if (message.author.id !== '257111476404224001') return message.reply('** هذا الأمر فقط لصاحب البوت و شكراًً **')
+                    client.user.setAvatar(argresult);
+                        message.channel.sendMessage(`**${argresult}** : تم تغير صورة البوت`);
+                    } else
+                    
+                    
+                    if (message.content.startsWith(prefix + 'watching')) {
+                    if (message.author.id !== '257111476404224001') return message.reply('** هذا الأمر فقط لصاحب البوت و شكراًً **')
+                        client.user.setActivity(argresult, {type : 'watching'});
+                     message.channel.sendMessage(`**${argresult}** : تم تغيير الووتشينق الى`)
+                    }
+                    
+});
+
+
 client.login('NDI3MDk0MzI1NDI0MjkxODYy.DZgA_g.t4K2Rl4CmqWgAGdBLgn7mcMs4sQ');
